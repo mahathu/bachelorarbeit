d2c5721d7af1e0256369350573013788e23b8b2b
Martin Hoffmann <maedhoff@ucsc.edu>
2020-06-30 20:05:28 +0200

    added spell check to word preprocessing

--- ELM/main.py
+++ ELM/main.py
@@ -4,6 +4,7 @@ including the word featurization stage using word2vec.
 """
 
 import pandas as pd
+import numpy as np
 from utilities import preprocess_texts, get_x_y, transform_texts
 from build_word2vec import build_w2v_model
 from gensim.models import Word2Vec
@@ -20,41 +21,61 @@ RASS_VAR_ID = 22086158
 Visite_ZNS_VAR_ID = 22085815
 Visite_Pflege_VAR_ID = 22085836
 
-# df = preprocess_texts(PAIRS_FILE_PATH, RASS_VAR_ID)
-df = pd.read_csv('data/pairs_RASS_spellcorrected.csv') # Alternative, wenn Eingabedaten schon bereinigt wurden
 
-# The texts in the dataframe above are already preprocessed/cleaned (see utiliites.py)
+df_rows = []
+for dims in [20, 40, 80]:
+    for min_count in [1,3,5]:
+        for window_size in [2,5]:
+            w2v_params = {
+                'WINDOW_SIZE': window_size,
+                'DIMENSIONS': dims,
+                'WORD_MIN_COUNT': min_count,
+                'TRAINING_EPOCHS': 10,
+            }
 
-w2v_model = build_w2v_model(df,
-                            window_size=5,
-                            dimensions=20,
-                            word_min_count=3, #5 is default
-                            epochs=10,
-                            use_skipgrams=1)
-# w2v_model = Word2Vec.load(W2V_MODEL_PATH) # Alternative, wenn Modell schon gebaut wurde
+            # df = preprocess_texts(PAIRS_FILE_PATH, RASS_VAR_ID)
+            df = pd.read_csv('data/pairs_RASS_spellcorrected.csv') # Alternative, wenn Eingabedaten schon bereinigt wurden
 
-X_texts, y = get_x_y(df, Visite_ZNS_VAR_ID, RASS_VAR_ID, 60)
+            # The texts in the dataframe above are already preprocessed/cleaned (see utiliites.py)
 
-X_matrix = transform_texts(w2v_model, X_texts)
-# X_matrix = pd.read_csv(X_MATRIX_PATH) # Alternative, wenn Eingabetexte schon vektorisiert wurden
+            w2v_model = build_w2v_model(df,
+                                        window_size=w2v_params['WINDOW_SIZE'], #5 is default
+                                        dimensions=w2v_params['DIMENSIONS'],
+                                        word_min_count=w2v_params['WORD_MIN_COUNT'], #5 is default
+                                        epochs=w2v_params['TRAINING_EPOCHS'],
+                                        use_skipgrams=1)
+            # w2v_model = Word2Vec.load(W2V_MODEL_PATH) # Alternative, wenn Modell schon gebaut wurde
 
-# Filter out rows that had invalid input data in the X matrix:
-n_rows_before = len(y)
-broken_rows_mask = X_matrix[X_matrix.columns[0]].notnull()
-X_matrix = X_matrix[broken_rows_mask]
-y = y[broken_rows_mask].squeeze()
-print(f"{n_rows_before-len(y)} rows where filtered because the X matrix contained no data (empty text or text with no known words)")
+            X_texts, y = get_x_y(df, Visite_ZNS_VAR_ID, RASS_VAR_ID, 90)
 
-# ========================= SVR (just for testing) ========================= #
-svr = SVR(cache_size=1024, C=10)
+            X_matrix = transform_texts(w2v_model, X_texts)
+            # X_matrix = pd.read_csv(X_MATRIX_PATH) # Alternative, wenn Eingabetexte schon vektorisiert wurden
 
-print("Running cross validation...")
+            # Filter out rows that had invalid input data in the X matrix:
+            n_rows_before = len(y)
+            broken_rows_mask = X_matrix[X_matrix.columns[0]].notnull()
+            X_matrix = X_matrix[broken_rows_mask]
+            y = y[broken_rows_mask].squeeze()
+            print(f"{n_rows_before-len(y)} rows where filtered because the X matrix contained no data (empty text or text with no known words)")
 
-scores = cross_validate(svr,
-                        X_matrix,
-                        y,
-                        cv=ShuffleSplit(n_splits=1, test_size=.2),
-                        scoring=['neg_mean_absolute_error', 'neg_root_mean_squared_error', 'r2']
-)
-for k in scores:
-    print(f"{k}: {scores[k]}")
+            # ========================= SVR (just for testing) ========================= #
+
+            for param in w2v_params:
+                print(f"{param}: {w2v_params[param]}")
+            
+            print(colored("="*80, 'cyan'))
+
+            for c in [.1, 1, 10]:
+                scores = cross_validate(SVR(cache_size=1024, C=c),
+                                        X_matrix,
+                                        y,
+                                        cv=ShuffleSplit(n_splits=5, test_size=.2),
+                                        scoring=['neg_mean_absolute_error']
+                )
+                
+                row = [dims, min_count, window_size, c, -1* np.mean(scores['test_neg_mean_absolute_error'])]
+                print(row)
+                df_rows.append(row)
+
+df = pd.DataFrame(df_rows, columns=['dimensions', 'word_min_count', 'window_size', 'svr_C', 'MAE'])
+df.to_csv('PERFORMANCE.csv')
